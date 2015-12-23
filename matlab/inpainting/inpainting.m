120e4c7d6f5857bb15fd3d70a90b5f83de641f58
Bob Vergauwen <bob.vergauwen@student.kuleuven.be>
2015-12-23 11:00:06 +0100

    Merged the inpainting files. added a smooth treshold.

--- matlab/inpainting/inpainting.m
+++ matlab/inpainting/inpainting.m
@@ -19,67 +19,60 @@ for i=1:7
         mask(64*i-n:64*i,64*j-n:64*j) =1;
     end
 end
-A_dist = A;
 A_dist = A.*(1-mask);
 %% Plotting image + distorsion
 close all
 colormap(cmap)
 image(A_dist);
-%%
-
-[C,S] = wavedec2(A_dist,2,'haar');
-coef = reshape(C,512,512);
-image(coef);
 
 %% Settings for the  wavelets.
-dwtmode('sym'); % sym,per
-wname = 'db5'; % bior4.4,haar,db1,db2
 
-t = wtree(A_dist,2,wname);
-%plot(t)
-%% Creating the treshold function
-SoftThresh = @(x,T)x.*max( 0, 1-T./max(abs(x),1e-10) );
-clf;
-T = linspace(-1,1,1000);
-%plot( T, SoftThresh(T,.5) );
-axis('equal');
+dwtmode('sym');     % Boundary conditions: sym,per
+wname = 'db5';      % Type of wavelet: bior4.4,haar,db1,db2
+Nb_levels = 20;     % Nb of resolution levels.
+
+%% Creating different treshold functions
+SoftThresh  = @(x,T) x.*max( 0, 1-T./max(abs(x),1e-10) );
+HardThresh  = @(x,T) x .* (abs(x) >= T);
+SmootThresh = @(x,T) -x.*exp(-(x/T).^4)+x;
 %% Setting up the transformation
-PsiS = @(f)wavedec2(f,20,wname);
+PsiS = @(f)wavedec2(f,Nb_levels,wname);
 Psi = @(C,S)waverec2(C,S,wname);
-
-%% Denoising test
-%imageplot( clamp(SoftThreshPsi(f0,.1)) );
-A_noise = randn(512)*20 +A;
-[C,S] = PsiS(A_noise);
-C = SoftThresh(C,20);
-colormap(cmap);
-A_denoise = Psi(C,S);
-subplot(1,2,1);
-image(A_noise)
-subplot(1,2,2);
-image(A_denoise)
 %% Implementatie van inpainting 
-% Minimaliseer: @(y) norm(psi(f)-y) + lambda *
+
+close all
+threshold = 'hard'; % 'soft' or 'hard' or 'smooth'
+
+delta = 10;         % threshold parameter
+maxit = 200;        % Max itterations of algorithm
+
 close all;
 B_n=A_dist;
+switch threshold
+    case 'soft'
+        threshold = @(C)SoftThresh(C,delta);
+    case 'hard'
+        threshold = @(C)HardThresh(C,delta);
+    case 'smooth'
+        threshold = @(C)SmootThresh(C,delta);
+    otherwise
+        error('thresholding kind unknown')
+end
 
-for n=1:200
+
+for n=1:maxit
     disp(n);
     [C,S] = PsiS(B_n);
-    C = SoftThresh(C,10);
+    C = threshold(C);
     B_np1 = (1-mask).*A_dist+mask.*Psi(C,S);
     B_n=B_np1;
     colormap(cmap)
     image(B_np1);
     pause(0.00001)
 end
-%%
+%% Outputting the results
 imwrite(A_dist,cmap,'lena_broke.gif')
 imwrite(B_np1,cmap,'lena_fixed.gif')
-
-
-
-
 
 
 
